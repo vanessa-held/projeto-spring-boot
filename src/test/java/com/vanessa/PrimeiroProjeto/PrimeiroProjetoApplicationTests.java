52aec7691ae0f71d2fbd4a1084a8ccb8b907d681
Vanessa Held <vanessaheld@ciandt.com>
2022-08-21 16:53:25 -0300

    primeiro commit

---
+++ src/test/java/com/vanessa/PrimeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.PrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
